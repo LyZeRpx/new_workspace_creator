0fa963f2282a80cfde67949f609ab12b76b0a59c
Liam Harfouche <liam.harfouche@epitech.eu>
2024-10-14 00:16:42 +0200

    [ADD] : check if rc exists

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,6 +2,12 @@
 echo What shell are you using ?
 read SHELL
 RCPATH=${HOME}/.${SHELL}rc
-echo 'alias import_lib="${HOME}/new_workspace_creator/import_lib.sh"' >> ${RCPATH}
-echo 'alias new_workspace="${HOME}/new_workspace_creator/repo_init.sh"' >> ${RCPATH}
-echo 'alias new_header="${HOME}/new_workspace_creator/header_maker.sh"' >> ${RCPATH}
+
+if [ -f "$RCPATH" ]; then
+    echo 'alias import_lib="${HOME}/new_workspace_creator/import_lib.sh"' >> ${RCPATH}
+    echo 'alias new_workspace="${HOME}/new_workspace_creator/repo_init.sh"' >> ${RCPATH}
+    echo 'alias new_header="${HOME}/new_workspace_creator/header_maker.sh"' >> ${RCPATH}
+    ${SHELL}
+else
+    echo "shell config not found."
+fi
