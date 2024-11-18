92dbf3b76b301959e7c3c8337a24a6138a2bb899
Liam Lequet <liam.harfouche@epitech.eu>
2024-11-18 12:43:43 +0100

    [ADD] : changed the headers dir to include

--- header_maker.sh
+++ header_maker.sh
@@ -8,6 +8,6 @@ else
 fi
 echo "Enter header name ?"
 read header_name
-cp -r ${REPO}/headers/project.h headers/$header_name.h
-sed -i "8i#ifndef ${header_name^^}_H" headers/$header_name.h
-sed -i "9i \ \ \ \ #define ${header_name^^}_H" headers/$header_name.h
+cp -r ${REPO}/include/project.h include/$header_name.h
+sed -i "8i#ifndef ${header_name^^}_H" include/$header_name.h
+sed -i "9i \ \ \ \ #define ${header_name^^}_H" include/$header_name.h
